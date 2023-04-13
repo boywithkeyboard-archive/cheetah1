89d5747458c1a77585e4c6349ae08a28d3ba5dbf
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-13 17:07:16 +0200

    feat(validator): add typebox

---
+++ validator/typebox.ts
@@ -0,0 +1,7 @@
+// @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.d.ts'
+import { Value as typebox } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.js/+esm'
+
+export default typebox
+
+// @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.d.ts'
+export { Type, Kind } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.js/+esm'
