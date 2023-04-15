39a4da93f586458dc64657259959c6249d313277
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-15 17:02:40 +0200

    style(validator): sort imports

--- validator/zod.ts
+++ validator/zod.ts
@@ -1,9 +1,9 @@
-import { Validator } from '../_validator.d.ts'
+import { Validator } from '../types.ts'
 
 const zod: Validator<'zod'> = {
   name: 'zod',
   check: undefined
 }
 
-export default zod
 export { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
+export default zod
