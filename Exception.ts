1966ac0c5a10f3cc690f0b10d765c3e6fefb41ce
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-15 17:04:29 +0200

    feat: add `Exception` class

---
+++ Exception.ts
@@ -0,0 +1,60 @@
+export class Exception {
+  public response
+
+  constructor(
+    error:
+      | 'Not Found'
+      | 'Access Denied'
+      | 'Something Went Wrong'
+      | 'Bad Request'
+      | 'Payload Too Large'
+      | 'Too Many Requests'
+      | 404
+      | 403
+      | 500
+      | 400
+      | 413
+      | 429
+  ) {
+    const message = typeof error === 'string'
+      ? error
+      : error === 404
+      ? 'Not Found'
+      : error === 400
+      ? 'Bad Request'
+      : error === 403
+      ? 'Access Denied'
+      : error === 413
+      ? 'Payload Too Large'
+      : error === 429
+      ? 'Too Many Requests'
+      : 'Something Went Wrong'
+
+    const code = typeof error === 'number'
+      ? error
+      : error === 'Access Denied'
+      ? 403
+      : error === 'Bad Request'
+      ? 400
+      : error === 'Not Found'
+      ? 404
+      : error === 'Payload Too Large'
+      ? 413
+      : error === 'Too Many Requests'
+      ? 429
+      : 500
+
+    this.response = new Response(
+      JSON.stringify({
+        message,
+        code
+      }),
+      {
+        headers: {
+          'content-type': ''
+        },
+        status: code
+      }
+    )
+  }
+}
