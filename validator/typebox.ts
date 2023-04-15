db8264ce3caad92ca74dc5b369edaab4dab1a7ef
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-15 16:24:34 +0200

    refactor(validator): edit extensions

--- validator/typebox.ts
+++ validator/typebox.ts
@@ -1,7 +1,12 @@
 // @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.d.ts'
-import { Value as typebox } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.js/+esm'
+import { Value } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.js/+esm'
+import { Validator } from '../_validator.d.ts'
+
+const typebox: Validator<'typebox'> = {
+  name: 'typebox',
+  check: Value.Check
+}
 
 export default typebox
-
 // @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.d.ts'
 export { Type, Kind } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.js/+esm'
--- validator/zod.ts
+++ validator/zod.ts
@@ -1,5 +1,9 @@
-import { z as zod } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
+import { Validator } from '../_validator.d.ts'
+
+const zod: Validator<'zod'> = {
+  name: 'zod',
+  check: undefined
+}
 
 export default zod
-
 export { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
