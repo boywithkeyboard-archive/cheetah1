f543419564e6148770cb1b6d6e9483f2ba1adf31
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-16 13:32:27 +0200

    refactor(validator): edit structure

--- validator/typebox.ts
+++ validator/typebox.ts
@@ -1,12 +1,13 @@
-// @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.d.ts'
+// @deno-types='https://esm.sh/@sinclair/typebox@0.27.4/value'
 import { Value } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/value/value.js/+esm'
-import { Validator } from '../types.ts'
 
-const typebox: Validator<'typebox'> = {
+const typebox = {
   name: 'typebox',
   check: Value.Check
 }
 
-// @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.d.ts'
+export type TypeBoxValidator = typeof typebox
+
+// @deno-types='https://esm.sh/@sinclair/typebox@0.27.4'
 export { Kind, Type } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.4/typebox.js/+esm'
 export default typebox
--- validator/zod.ts
+++ validator/zod.ts
@@ -1,9 +1,9 @@
-import { Validator } from '../types.ts'
-
-const zod: Validator<'zod'> = {
+const zod = {
   name: 'zod',
   check: undefined
 }
+
+export type ZodValidator = typeof zod
 
 export { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
 export default zod
