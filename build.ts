530707217bef0b7119ca25f56f5f39d03483bd12
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-20 12:57:53 +0200

    fix: make build script work

--- build.ts
+++ build.ts
@@ -1,17 +1,17 @@
 import { build, stop } from 'https://deno.land/x/esbuild@v0.17.17/mod.js'
-import { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts'
+
+await Deno.run({ cmd: ['deno', 'bundle', '-q', Deno.args[0] ?? './mod.ts', Deno.args[1] ?? './mod.js'] }).status()
 
 await build({
-  // @ts-ignore:
-  plugins: [denoPlugin()],
-  entryPoints: [Deno.args[0] ?? './mod.ts'],
+  entryPoints: [Deno.args[1] ?? './mod.js'],
   bundle: true,
   minify: true,
   format: 'esm',
+  allowOverwrite: true,
   banner: {
-    js: '// deno-lint-ignore-file',
+    js: '// deno-fmt-ignore-file\n// deno-lint-ignore-file',
   },
-  outfile: Deno.args[1] ?? './worker.js',
+  outfile: Deno.args[1] ?? './mod.js',
 })
 
 stop()
