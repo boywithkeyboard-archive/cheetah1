10e83327ff5996879fc77b6382bf68a51e2c23c4
GitHub <noreply@github.com>
2023-04-26 01:12:49 +0000

    bump dependencies

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { z, ZodObject, ZodType } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
-import { TObject, TSchema, Static as TypeBoxStatic } from 'https://esm.sh/@sinclair/typebox@0.28.5'
+import { TObject, TSchema, Static as TypeBoxStatic } from 'https://esm.sh/@sinclair/typebox@0.28.6'
 import typebox from './validator/typebox.ts'
 import zod from './validator/zod.ts'
 
