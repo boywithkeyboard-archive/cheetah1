668405877c4bdea3460bf749af49acca285231af
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-30 12:39:06 +0200

    feat: add `new` script

---
+++ new.ts
@@ -0,0 +1,42 @@
+import { parse } from 'https://deno.land/std@v0.185.0/flags/mod.ts'
+import { brightRed } from 'https://deno.land/std@v0.185.0/fmt/colors.ts'
+import { ensureDir } from 'https://deno.land/std@v0.185.0/fs/ensure_dir.ts'
+
+const args = parse(Deno.args)
+
+async function download(prefix: string, ...paths: string[]) {
+  for (const path of paths) {
+    const response = await fetch(`https://raw.githubusercontent.com/azurystudio/cheetah/dev/templates/${prefix}/${path}`)
+
+    if (path.includes('/'))
+      await ensureDir(path.substring(1, path.lastIndexOf('/')))
+
+    await Deno.writeTextFile(path, await response.text())
+  }
+}
+
+if (args.template === 'deno') {
+  await download(
+    'deno',
+    
+    '.github/workflows/bump.yml',
+    '.vscode/settings.json',
+    'readme.md',
+    'deno.json',
+    'mod.ts'
+  )
+} else if (args.template === 'cloudflare') {
+  await download(
+    'cloudflare',
+
+    '.github/workflows/bump.yml',
+    '.vscode/settings.json',
+    'readme.md',
+    'deno.json',
+    'mod.ts',
+    'wrangler.toml',
+    '.gitignore'
+  )
+} else {
+  console.error(brightRed('cannot find template'))
+}
