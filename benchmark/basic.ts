8f743c17c62741d218aae124eff9c6f6be3ee2a2
GitHub <noreply@github.com>
2023-05-03 01:14:19 +0000

    bump dependencies

--- benchmark/basic.ts
+++ benchmark/basic.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'https://deno.land/x/hono@v3.1.7/mod.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.1.8/mod.ts'
 import { Application, Router } from 'https://deno.land/x/oak@v12.4.0/mod.ts'
 import { Router as IttyRouter } from 'https://esm.sh/itty-router@3.0.12'
 import Cheetah from '../mod.ts'
