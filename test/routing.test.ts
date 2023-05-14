271f74e462c85d7662f8e1d7a670e9c23b0edd1c
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-05-15 00:58:00 +0200

    test: edit test suite for routing

--- test/routing.test.ts
+++ test/routing.test.ts
@@ -1,24 +1,26 @@
 import { assertEquals } from 'https://deno.land/std@0.187.0/testing/asserts.ts'
 import cheetah, { Collection } from '../mod.ts'
 
-Deno.test('Nesting/Routing', async () => {
+Deno.test('Nesting/Routing', async t => {
   const collection = new Collection()
     .get('/', () => 'nested root route')
     .get('/nestedroute', () => 'nested route')
 
-  // root with base path
-  const r1 = new cheetah({
-    base: '/api'
+  await t.step('Root with Base Path', async () => {
+    const r1 = new cheetah({
+      base: '/api'
+    })
+      .use('/collection', collection)
+  
+    assertEquals(await (await r1.fetch(new Request('http://localhost:3000/api/collection'))).text(), 'nested root route')
+    assertEquals(await (await r1.fetch(new Request('http://localhost:3000/api/collection/nestedroute'))).text(), 'nested route')
   })
-    .use('/collection', collection)
 
-  assertEquals(await (await r1.fetch(new Request('http://localhost:3000/api/collection'))).text(), 'nested root route')
-  assertEquals(await (await r1.fetch(new Request('http://localhost:3000/api/collection/nestedroute'))).text(), 'nested route')
+  await t.step('Root without Base Path', async () => {
+    const r2 = new cheetah()
+      .use('/collection', collection)
 
-  // root without base path
-  const r2 = new cheetah()
-    .use('/collection', collection)
-
-  assertEquals(await (await r2.fetch(new Request('http://localhost:3000/collection'))).text(), 'nested root route')
-  assertEquals(await (await r2.fetch(new Request('http://localhost:3000/collection/nestedroute'))).text(), 'nested route')
+    assertEquals(await (await r2.fetch(new Request('http://localhost:3000/collection'))).text(), 'nested root route')
+    assertEquals(await (await r2.fetch(new Request('http://localhost:3000/collection/nestedroute'))).text(), 'nested route')
+  })
 })
