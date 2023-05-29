3e013e3416ddb0793228f1013c88f5a7e3629442
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-05-29 17:11:14 +0200

    refactor(build): minor changes to the build script

--- build.ts
+++ build.ts
@@ -1,47 +1,55 @@
+import { parse } from 'https://deno.land/std@0.189.0/flags/mod.ts'
+import { brightRed, gray } from 'https://deno.land/std@0.189.0/fmt/colors.ts'
 import { build, stop } from 'https://deno.land/x/esbuild@v0.17.19/mod.js'
 
-async function isImportMap(path: string) {
-  try {
-    const content = await Deno.readTextFile(path)
-
-    if (!content)
+try {
+  const { _ } = parse(Deno.args)
+  
+  const isImportMap = async (path: string) => {
+    try {
+      const content = await Deno.readTextFile(path)
+  
+      if (!content)
+        return false
+  
+      return JSON.parse(content).imports !== undefined
+    } catch (_) {
       return false
-
-    return JSON.parse(content).imports !== undefined
-  } catch (_) {
-    return false
+    }
   }
+
+  let options: string[] = []
+
+  if (await isImportMap('deno.json'))
+    options = ['--config', 'deno.json']
+  else if (await isImportMap('deno.jsonc'))
+    options = ['--config', 'deno.jsonc']
+  else if (await isImportMap('import_map.json'))
+    options = ['--import-map', 'deno.json']
+  else if (await isImportMap('importMap.json'))
+    options = ['--import-map', 'importMap.json']
+  else if (await isImportMap('imports.json'))
+    options = ['--import-map', 'imports.json']
+
+  const cmd = new Deno.Command('deno', {
+    args: ['bundle', '-q', ...options, (_[0] as string) ?? './mod.ts', (_[1] as string) ?? './mod.js']
+  })
+  
+  await cmd.output()
+  
+  await build({
+    entryPoints: [(_[1] as string) ?? './mod.js'],
+    bundle: true,
+    minify: true,
+    format: 'esm',
+    allowOverwrite: true,
+    banner: {
+      js: '// deno-fmt-ignore-file\n// deno-lint-ignore-file',
+    },
+    outfile: (_[1] as string) ?? './mod.js'
+  })
+  
+  stop()
+} catch (err) {
+  console.error(gray(`${brightRed('error')} - ${typeof err === 'string' ? err : 'unknown exception'}`))
 }
-
-let args: string[] = []
-
-if (await isImportMap('deno.json'))
-  args = ['--config', 'deno.json']
-else if (await isImportMap('deno.jsonc'))
-  args = ['--config', 'deno.jsonc']
-else if (await isImportMap('import_map.json'))
-  args = ['--import-map', 'deno.json']
-else if (await isImportMap('importMap.json'))
-  args = ['--import-map', 'importMap.json']
-else if (await isImportMap('imports.json'))
-  args = ['--import-map', 'imports.json']
-
-const cmd = new Deno.Command('deno', {
-  args: ['bundle', '-q', ...args, Deno.args[0] ?? './mod.ts', Deno.args[1] ?? './mod.js']
-})
-
-await cmd.output()
-
-await build({
-  entryPoints: [Deno.args[1] ?? './mod.js'],
-  bundle: true,
-  minify: true,
-  format: 'esm',
-  allowOverwrite: true,
-  banner: {
-    js: '// deno-fmt-ignore-file\n// deno-lint-ignore-file',
-  },
-  outfile: Deno.args[1] ?? './mod.js',
-})
-
-stop()
