72325a7c551a18287bd6a9ea69f1561e2cc9171f
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-11 02:54:45 +0200

    refactor: use `URLPattern` api (#75)

--- core/Router.ts
+++ core/Router.ts
@@ -1,39 +1,33 @@
+import { Method } from './_base.ts'
 import { Route } from './_handler.ts'
 
 export class Router {
-  #routes: [string, RegExp, Route[]][] = []
+  #r: [Uppercase<Method>, URLPattern, Route[]][] = []
 
-  add(method: string, pathname: string, handler: Route[]) {
-    this.#routes.push([
+  add(method: Uppercase<Method>, pathname: string, handler: Route[]) {
+    this.#r.push([
       method,
-      RegExp(`^${pathname
-          .replace(/(\/?)\*/g, '($1.*)?')
-          .replace(/(\/$)|((?<=\/)\/)/, '')
-          .replace(/(:(\w+)\+)/, '(?<$2>.*)')
-          .replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/]+)$2$3')
-          .replace(/\.(?=[\w(])/, '\\.')
-        //.replace(/\)\.\?\(([^\[]+)\[\^/g, '?)\\.?($1(?<=\\.)[^\\.')
-      }/*$`),
+      new URLPattern({ pathname }),
       handler,
     ])
   }
 
   match(method: string, pathname: string, preflight: boolean) {
-    for (let i = 0; i < this.#routes.length; ++i) {
+    for (let i = 0; i < this.#r.length; ++i) {
       if (
-        method === this.#routes[i][0] ||
+        method === this.#r[i][0] ||
         method === 'OPTIONS' ||
-        preflight && method === 'HEAD' && this.#routes[i][0] === 'GET'
+        preflight && method === 'HEAD' && this.#r[i][0] === 'GET'
       ) {
-        const matches = pathname.match(this.#routes[i][1])
+        const result = this.#r[i][1].exec({ pathname })
 
-        if (!matches) {
+        if (!result) {
           continue
         }
 
         return {
-          handlers: this.#routes[i][2],
-          params: matches.groups ?? {},
+          handlers: this.#r[i][2],
+          params: result.pathname.groups,
         }
       }
     }
