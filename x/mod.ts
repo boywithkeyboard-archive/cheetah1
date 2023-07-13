91f93c2afac23aedb1d04d78f978582fc1993d63
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-13 15:48:47 +0200

    feat(x): `LocationData` module (#97)

--- x/mod.ts
+++ x/mod.ts
@@ -1,5 +1,6 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 export { Cache } from './Cache.ts'
+export { LocationData } from './LocationData.ts'
 export { env } from './env.ts'
 export { jsx } from './jsx.tsx'
 export { createKey, default as jwt, importKey } from './jwt.ts'
