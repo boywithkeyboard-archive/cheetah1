84439f7a667d18be085e4909f4d6ec4f91136c91
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-13 03:30:32 +0200

    BREAKING: remove `cache` option (#92)

--- Collection.ts
+++ Collection.ts
@@ -3,13 +3,6 @@ import { base, Method } from './base.ts'
 import { HandlerOrSchema } from './handler.ts'
 
 export class Collection extends base<Collection>() {
-  #cache:
-    | false
-    | {
-      maxAge: number
-    }
-    | undefined
-
   #cors:
     | string
     | undefined
@@ -21,19 +14,8 @@ export class Collection extends base<Collection>() {
   ][]
 
   constructor({
-    cache,
     cors,
   }: {
-    /**
-     * Duration in seconds for how long a response should be cached.
-     *
-     * @since v0.11
-     */
-    cache?:
-      | false
-      | {
-        maxAge: number
-      }
     /**
      * Enable Cross-Origin Resource Sharing (CORS) for this collection by setting a origin, e.g. `*`.
      *
@@ -42,11 +24,7 @@ export class Collection extends base<Collection>() {
     cors?: string
   } = {}) {
     super((method, pathname, handlers) => {
-      if ((this.#cache || this.#cors) && typeof handlers[0] !== 'function') {
-        if (handlers[0].cache === undefined && this.#cache !== undefined) {
-          handlers[0].cache = this.#cache
-        }
-
+      if (this.#cors && typeof handlers[0] !== 'function') {
         if (!handlers[0].cors) {
           handlers[0].cors = this.#cors
         }
@@ -57,9 +35,7 @@ export class Collection extends base<Collection>() {
       return this
     })
 
-    this.routes = []
-
-    this.#cache = cache
     this.#cors = cors
+    this.routes = []
   }
 }
