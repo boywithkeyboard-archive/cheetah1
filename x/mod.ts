a168f692813245d8e015dd0c69157522594f6d59
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-13 15:43:36 +0200

    feat(x): `jsx` module (#96)
    * feat(x): jsx module
    
    * move x tests to single file

--- x/mod.ts
+++ x/mod.ts
@@ -1,6 +1,7 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 export { Cache } from './Cache.ts'
 export { env } from './env.ts'
+export { jsx } from './jsx.tsx'
 export { createKey, default as jwt, importKey } from './jwt.ts'
 export { otp } from './otp.ts'
 export { sendMail } from './sendMail.ts'
