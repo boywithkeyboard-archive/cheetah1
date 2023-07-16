50d0381d488ad95786cf9cecab49fddd1dd88781
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-16 16:29:25 +0200

    add lowercase files

---
+++ x/cache.ts
@@ -0,0 +1,115 @@
+// Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
+import { encode } from 'https://deno.land/std@0.194.0/encoding/base64.ts'
+import { Context } from '../mod.ts'
+
+export class Cache {
+  #cache: globalThis.Cache | null
+  #context
+  #maxAge
+  #name
+
+  constructor(c: Context, {
+    maxAge = 600,
+    name = 'cheetah',
+  }: {
+    /**
+     * Duration in seconds for how long a response should be cached.
+     *
+     * @default 600
+     */
+    maxAge?: number
+    /** A unique name for your cache. */
+    name?: string
+  } = {}) {
+    this.#cache = null
+    this.#context = c
+    this.#maxAge = maxAge
+    this.#name = name
+  }
+
+  async set(key: string, data: string | Record<string, unknown> | Uint8Array) {
+    if (this.#cache === null) {
+      this.#cache = await caches.open(this.#name)
+    }
+
+    this.#context.waitUntil(
+      this.#cache.put(
+        `https://${this.#name}.com/${encode(key)}`,
+        new Response(
+          typeof data === 'string' || data instanceof Uint8Array
+            ? data
+            : JSON.stringify(data),
+          {
+            headers: {
+              'cache-control': `max-age=${this.#maxAge}`,
+            },
+          },
+        ),
+      ),
+    )
+  }
+
+  get<T extends Record<string, unknown> = Record<string, unknown>>(
+    key: string,
+    type: 'json',
+  ): Promise<T | undefined>
+  get<T extends string = string>(
+    key: string,
+    type: 'string',
+  ): Promise<T | undefined>
+  get<T extends Uint8Array = Uint8Array>(
+    key: string,
+    type: 'buffer',
+  ): Promise<T | undefined>
+
+  async get<T extends Record<string, unknown> | string | Uint8Array>(
+    key: string,
+    type: 'string' | 'json' | 'buffer' = 'string',
+  ): Promise<T | undefined> {
+    if (this.#cache === null) {
+      this.#cache = await caches.open(this.#name)
+    }
+
+    try {
+      const result = await this.#cache.match(
+        `https://${this.#name}.com/${encode(key)}`,
+      )
+
+      if (!result) {
+        return undefined
+      }
+
+      const data = type === 'string'
+        ? await result.text()
+        : type === 'json'
+        ? await result.json()
+        : new Uint8Array(await result.arrayBuffer())
+
+      return data
+    } catch (_err) {
+      return undefined
+    }
+  }
+
+  async has(key: string) {
+    if (this.#cache === null) {
+      this.#cache = await caches.open(this.#name)
+    }
+
+    const result = await this.#cache.match(
+      `https://${this.#name}.com/${encode(key)}`,
+    )
+
+    return result !== undefined
+  }
+
+  async delete(key: string) {
+    if (this.#cache === null) {
+      this.#cache = await caches.open(this.#name)
+    }
+
+    return await this.#cache.delete(
+      `https://${this.#name}.com/${encode(key)}`,
+    )
+  }
+}
---
+++ x/location_data.ts
@@ -0,0 +1,146 @@
+// Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
+import { IncomingRequestCfProperties } from 'https://cdn.jsdelivr.net/npm/@cloudflare/workers-types@4.20230710.0/index.ts'
+import { Context } from '../mod.ts'
+
+type CloudflareRequest = Request & {
+  cf: IncomingRequestCfProperties
+}
+
+/**
+ * Inspect the geolocation data of the incoming request.
+ *
+ * You must either deploy your app to [Cloudflare Workers](https://developers.cloudflare.com/workers/runtime-apis/request/#incomingrequestcfproperties) or use [Cloudflare as a proxy](https://developers.cloudflare.com/support/network/configuring-ip-geolocation/) to use the `LocationData` API.
+ */
+export class LocationData {
+  #c: Context
+
+  constructor(c: Context) {
+    this.#c = c
+  }
+
+  /**
+   * The city the request originated from.
+   *
+   * @example 'Austin'
+   */
+  get city() {
+    const city = (this.#c.req.raw as CloudflareRequest).cf.city
+
+    if (!city && this.#c.__app.proxy === 'cloudflare') {
+      return this.#c.req.headers['cf-ipcity']
+    }
+
+    return city
+  }
+
+  /**
+   * If known, the ISO 3166-2 name for the first level region associated with the IP address of the incoming request.
+   *
+   * @example 'Texas'
+   */
+  get region() {
+    return (this.#c.req.raw as CloudflareRequest).cf.region
+  }
+
+  /**
+   * The [ISO 3166-1 Alpha 2](https://www.iso.org/iso-3166-country-codes.html) country code the request originated from.
+   *
+   * If you're using CLoudflare Workers and your worker is [configured to accept TOR connections](https://support.cloudflare.com/hc/en-us/articles/203306930-Understanding-Cloudflare-Tor-support-and-Onion-Routing), this may also be `T1`, indicating a request that originated over TOR.
+   *
+   * If Cloudflare is unable to determine where the request originated this property is omitted.
+   *
+   * @example 'GB'
+   */
+  get country(): IncomingRequestCfProperties['country'] {
+    const country = (this.#c.req.raw as CloudflareRequest).cf.country
+
+    if (!country && this.#c.__app.proxy === 'cloudflare') {
+      return this.#c.req
+        .headers['cf-ipcountry'] as IncomingRequestCfProperties['country']
+    }
+
+    return country
+  }
+
+  /**
+   * A two-letter code indicating the continent the request originated from.
+   *
+   * @example 'NA'
+   */
+  get continent(): IncomingRequestCfProperties['continent'] {
+    const continent = (this.#c.req.raw as CloudflareRequest).cf.continent
+
+    if (!continent && this.#c.__app.proxy === 'cloudflare') {
+      return this.#c.req
+        .headers['cf-ipcontinent'] as IncomingRequestCfProperties['continent']
+    }
+
+    return continent
+  }
+
+  /**
+   * If known, the ISO 3166-2 code for the first-level region associated with the IP address of the incoming request.
+   *
+   * @example 'TX'
+   */
+  get regionCode(): IncomingRequestCfProperties['regionCode'] {
+    return (this.#c.req.raw as CloudflareRequest).cf.regionCode
+  }
+
+  /**
+   * Latitude of the incoming request.
+   *
+   * @example '30.27130'
+   */
+  get latitude(): IncomingRequestCfProperties['latitude'] {
+    const latitude = (this.#c.req.raw as CloudflareRequest).cf.latitude
+
+    if (!latitude && this.#c.__app.proxy === 'cloudflare') {
+      return this.#c.req.headers['cf-iplatitude']
+    }
+
+    return latitude
+  }
+
+  /**
+   * Longitude of the incoming request.
+   *
+   * @example '-97.74260'
+   */
+  get longitude(): IncomingRequestCfProperties['longitude'] {
+    const longitude = (this.#c.req.raw as CloudflareRequest).cf.longitude
+
+    if (!longitude && this.#c.__app.proxy === 'cloudflare') {
+      return this.#c.req.headers['cf-iplongitude']
+    }
+
+    return longitude
+  }
+
+  /**
+   * Postal code of the incoming request.
+   *
+   * @example '78701'
+   */
+  get postalCode(): IncomingRequestCfProperties['postalCode'] {
+    return (this.#c.req.raw as CloudflareRequest).cf.postalCode
+  }
+
+  /**
+   * Timezone of the incoming request.
+   *
+   * @example 'America/Chicago'
+   */
+  get timezone(): IncomingRequestCfProperties['timezone'] {
+    return (this.#c.req.raw as CloudflareRequest).cf.timezone
+  }
+
+  /**
+   * The three-letter [IATA](https://en.wikipedia.org/wiki/IATA_airport_code) airport code of the data center that the request hit.
+   *
+   * @example 'DFW'
+   */
+  get datacenter(): IncomingRequestCfProperties['colo'] {
+    return (this.#c.req.raw as CloudflareRequest).cf.colo
+  }
+}
