9bab16a17ad078ebdb56fc5a31cd1435ad015758
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-07-16 15:45:21 +0200

    bench: don't update readme

--- bench/compare.ts
+++ bench/compare.ts
@@ -52,41 +52,6 @@ const current = await bench(
 
 const canary = await bench('../mod.ts')
 
-const readme = await Deno.readTextFile('./readme.md')
-
-let table =
-  '| Channel | Avg | Max | p75 | p90 | p95 | p99 |\n| --- | --- | --- | --- | --- | --- | --- |\n'
-
-table +=
-  `| ◆ [**current**](https://deno.land/x/cheetah@${latestVersion}) <sup>${latestVersion}</sup> | ${
-    Math.round(current.rps.mean)
-  } | ${Math.round(current.rps.max)} | ${
-    Math.round(current.rps.percentiles.p75)
-  } | ${Math.round(current.rps.percentiles.p90)} | ${
-    Math.round(current.rps.percentiles.p95)
-  } | ${Math.round(current.rps.percentiles.p99)} |\n`
-
-table += `| ◇ [**canary**](https://deno.land/x/cheetah${
-  Deno.args[0] ? `@${Deno.args[0]}` : ''
-})${Deno.args[0] ? ` <sup>${Deno.args[0]}</sup>` : ''} | ${
-  Math.round(canary.rps.mean)
-} | ${Math.round(canary.rps.max)} | ${
-  Math.round(canary.rps.percentiles.p75)
-} | ${Math.round(canary.rps.percentiles.p90)} | ${
-  Math.round(canary.rps.percentiles.p95)
-} | ${Math.round(canary.rps.percentiles.p99)} |`
-
-await Deno.writeTextFile(
-  './readme.md',
-  readme.replace(
-    readme.substring(
-      readme.indexOf('[//]: bstart\n\n') + '[//]: bstart\n\n'.length,
-      readme.indexOf('\n\n[//]: bend'),
-    ),
-    table,
-  ),
-)
-
 console.clear()
 
 console.log(
