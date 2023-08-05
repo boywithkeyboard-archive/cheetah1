8184bb0f529c8e8f48cb084e5528b02d100c6ccc
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-05 22:12:08 +0200

    refactor(setup): download files to cwd (#135)

--- setup.ts
+++ setup.ts
@@ -1,59 +1,95 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
-import {
-  brightGreen,
-  brightRed,
-  gray,
-} from 'https://deno.land/std@0.195.0/fmt/colors.ts'
-import { Select } from 'https://deno.land/x/cliffy@v0.25.7/prompt/select.ts'
-import { decompress } from 'https://deno.land/x/zip@v1.2.5/mod.ts'
+import { ensureFile } from 'https://deno.land/std@0.197.0/fs/ensure_file.ts'
+import { Select } from 'https://deno.land/x/cliffy@v0.25.7/mod.ts'
+
+type VSCodeSettings = {
+  files: string[]
+  vscode: Record<string, unknown>
+  imports: string[]
+}
 
 async function setup() {
-  const runtime = await Select.prompt({
-    message: 'What runtime do you use?',
+  const platform = await Select.prompt({
+    message: 'Where do you plan to deploy your app?',
     options: [
-      { name: 'Deno', value: 'deno' },
-      { name: 'Cloudflare Workers', value: 'cloudflare' },
+      { name: 'Deno 🦕', value: 'deno' },
+      { name: 'Cloudflare Workers ⚡', value: 'cloudflare' },
     ],
   })
 
-  const template = 'template-{runtime}-basic'
+  let url =
+    `https://raw.githubusercontent.com/boywithkeyboard/templates/dev/${platform}/`
 
-  // const template = await Select.prompt({
-  //   message: 'What template do you want to use?',
-  //   options: [
-  //     { name: 'Basic', value: 'template-{runtime}-basic' }
-  //   ]
-  // })
+  url += 'starter/'
 
-  const response = await fetch(
-    `https://github.com/cheetahland/${
-      template.replace('{runtime}', runtime)
-    }/zipball/main`,
+  const meta: VSCodeSettings = JSON.parse(
+    await (await fetch(url + 'meta.json')).text(),
   )
 
-  if (!response.body) {
-    return console.error(
-      gray(`${brightRed('error')} - Failed to fetch template!`),
+  const importMap: Record<string, string> = JSON.parse(
+    await (await fetch(
+      'https://raw.githubusercontent.com/boywithkeyboard/templates/dev/deno.json',
+    )).text(),
+  ).imports
+
+  for (const f of meta.files) {
+    const res = await fetch(url + f)
+
+    await ensureFile(f)
+    await Deno.writeTextFile(f, await res.text())
+  }
+
+  const imports = {}
+
+  for (const i of meta.imports) {
+    // @ts-ignore:
+    imports[i] = importMap[i]
+  }
+
+  await ensureFile('.vscode/settings.json')
+  await Deno.writeTextFile('.vscode/settings.json', JSON.stringify(meta.vscode))
+
+  if (platform === 'cloudflare') {
+    await Deno.writeTextFile(
+      'wrangler.toml',
+      `name = "<name>"
+account_id = "<account_id>"
+route = "<route>"
+compatibility_date = "2023-03-24"
+    
+[build]
+command = "deno task build"`,
     )
   }
 
-  await Deno.writeFile('./template.zip', response.body)
+  const version =
+    (await (await fetch('https://apiland.deno.dev/v2/modules/cheetah')).json())
+      .latest_version
 
-  const isUnzippingSuccessful = await decompress('./template.zip', './')
-
-  if (!isUnzippingSuccessful) {
-    return console.error(
-      gray(`${brightRed('error')} - Failed to unzip template!`),
-    )
-  }
-
-  await Deno.remove('./template.zip')
-
-  console.info(
-    gray(
-      `${brightGreen('success')} - please read the readme.md to get started.`,
+  await Deno.writeTextFile(
+    'deno.json',
+    JSON.stringify(
+      {
+        fmt: {
+          semiColons: false,
+          singleQuote: true,
+        },
+        imports,
+        tasks: {
+          build: `deno run -A https://deno.land/x/cheetah@${version}/build.ts`,
+        },
+      },
+      null,
+      2,
     ),
   )
+
+  const command = new Deno.Command('deno', {
+    args: ['fmt', '--single-quote', '--no-semicolons'],
+    stdout: 'piped',
+  })
+
+  command.spawn()
 }
 
 if (import.meta.main) {
