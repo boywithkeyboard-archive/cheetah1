a5d8a50d52645d067105ffadce00054ad362ab21
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-07 19:54:14 +0200

    fix isSignedIn method

--- oauth/is_signed_in.ts
+++ oauth/is_signed_in.ts
@@ -2,5 +2,15 @@ import { Context } from '../context.ts'
 import { getSessionId } from './get_session_id.ts'
 
 export async function isSignedIn(c: Context): Promise<boolean> {
-  return await getSessionId(c) !== undefined
+  if (!c.__app.oauth) {
+    throw new Error('Please configure the oauth module for your app!')
+  }
+
+  const sessionId = await getSessionId(c)
+
+  if (!sessionId) {
+    return false
+  }
+
+  return await c.__app.oauth.store.hasSession(c, sessionId)
 }
