960a15fd2263fc730fabb703538b393fe0884418
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-13 22:57:45 +0200

    add `isSignedIn` method

--- oauth/is_signed_in.ts
+++ oauth/is_signed_in.ts
@@ -2,16 +2,12 @@
 import { Context } from '../context.ts'
 import { getSessionId } from './get_session_id.ts'
 
+/**
+ * Check if the user is logged in.
+ * 
+ * @namespace oauth
+ * @since v1.3
+ */
 export async function isSignedIn(c: Context): Promise<boolean> {
-  if (!c.__app.oauth) {
-    throw new Error('Please configure the oauth module for your app!')
-  }
-
-  const sessionId = await getSessionId(c)
-
-  if (!sessionId) {
-    return false
-  }
-
-  return await c.__app.oauth.store.hasSession(c, sessionId)
+  return await getSessionId(c) !== undefined
 }
