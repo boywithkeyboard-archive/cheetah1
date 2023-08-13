84053f913c5570709f51454e50957ac5ca05f8ff
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-13 23:38:03 +0200

    add `getSessionId` method

--- oauth/get_session_id.ts
+++ oauth/get_session_id.ts
@@ -1,13 +1,20 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 import { Context } from '../context.ts'
-import { env } from '../x/env.ts'
+import { getVariable } from '../x/env.ts'
 import { verify as jwtVerify } from '../x/jwt.ts'
+import { OAuthSessionToken } from './types.ts'
 
+/**
+ * Get the session identifier and verify it.
+ *
+ * @namespace oauth
+ * @since v1.3
+ */
 export async function getSessionId(c: Context): Promise<string | undefined> {
   if (!c.__app.oauth) {
     throw new Error('Please configure the oauth module for your app!')
   }
-  
+
   const header = c.req.headers.authorization
 
   if (!header || /^bearer\s[a-zA-Z0-9-_.]+$/.test(header) === false) {
@@ -16,23 +23,24 @@ export async function getSessionId(c: Context): Promise<string | undefined> {
 
   const token = header.split(' ')[1]
 
-  const e = env<{
-    jwtSecret?: string
-    jwt_secret?: string
-    JWT_SECRET?: string
-  }>(c)
-
-  const payload = await jwtVerify<{ sessionId: string }>(
+  const payload = await jwtVerify<OAuthSessionToken>(
     token,
-    e.jwtSecret ?? e.jwt_secret ?? e.JWT_SECRET as string,
-    { audience: 'oauth' },
+    getVariable(c, 'JWT_SECRET') ?? getVariable(c, 'jwt_secret') ??
+      getVariable(c, 'jwtSecret'),
+    { audience: 'oauth:session' },
   )
 
   if (!payload) {
     return
   }
 
-  if (await c.__app.oauth.store.hasSession(c, payload.sessionId)) {
-    return payload.sessionId
+  if (payload.ip !== c.req.ip) {
+    await c.__app.oauth.store.delete(c, payload.identifier)
+
+    return
+  }
+
+  if (await c.__app.oauth.store.has(c, payload.identifier)) {
+    return payload.identifier
   }
 }
