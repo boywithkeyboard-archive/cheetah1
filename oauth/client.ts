d03626fb80ee3f4988db4ddbb74e5c8fa13db2fb
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-13 23:03:57 +0200

    add license header

--- oauth/client.ts
+++ oauth/client.ts
@@ -1,3 +1,4 @@
+// Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 import { Preset } from 'https://deno.land/x/authenticus@v2.0.3/createPreset.ts'
 import {
   GitHub as githubPreset,
