46649db477bebe454b6762121087af96aee1d19e
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-13 22:37:25 +0200

    add sign out function

--- oauth/sign_out.ts
+++ oauth/sign_out.ts
@@ -1,14 +1,31 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 import { Context } from '../context.ts'
-import { getSessionId } from './get_session_id.ts'
+import { getSessionData } from './get_session_data.ts'
 
 /**
+ * Sign the user out if they're logged in.
+ *
+ * @namespace oauth
  * @since v1.3
  */
 export async function signOut(c: Context) {
-  const sessionId = await getSessionId(c)
+  if (!c.__app.oauth) {
+    throw new Error('Please configure the oauth module for your app!')
+  }
 
-  if (!sessionId) {
+  const data = await getSessionData(c)
+
+  if (!data) {
     return
+  }
+
+  try {
+    await c.__app.oauth.store.delete(c, data.identifier)
+  } catch (_err) {
+    //
+  }
+
+  if (typeof c.__app.oauth.onSignOut === 'function') {
+    await c.__app.oauth.onSignOut(c, data)
   }
 }
