c2a843a224a3b204fa4dbd979c9847a9a7dce1e1
ivy <ivy@not-ivy.dev>
2023-08-18 17:55:55 -0700

    test: add testing for tw-in-jsx

--- x/x.test.tsx
+++ x/x.test.tsx
@@ -1,9 +1,10 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 /** @jsx h */
 import { cheetah } from '../cheetah.ts'
-import { assertEquals, assertInstanceOf } from '../test/deps.ts'
+import { assert, assertEquals, assertInstanceOf, defineConfig, presetAutoPrefix, presetTailwind, DOMParser } from '../test/deps.ts'
 import { h, jsx } from './jsx.tsx'
 import { createKey, importKey, sign, verify } from './jwt.ts'
+import install from './tw.ts'
 
 // TODO delete at v2.0
 
@@ -20,34 +21,64 @@ Deno.test('x', async (t) => {
     assertEquals(await verify(token, cryptoKey) !== undefined, true)
   })
 
-  await t.step('jsx', async () => {
-    const app = new cheetah()
+  await t.step('jsx', async (t) => {
+    await t.step('plain', async () => {
+      const app = new cheetah();
 
-    function Custom() {
-      return <h1>hello world</h1>
-    }
+      function Custom() {
+        return <h1>hello world</h1>
+      }
 
-    app.get('/a', (c) => jsx(c, Custom))
-    app.get('/b', (c) => jsx(c, <Custom />))
+      app.get('/a', (c) => jsx(c, Custom))
+      app.get('/b', (c) => jsx(c, <Custom />))
+  
+      const a = await app.fetch(new Request('http://localhost/a'))
+      const b = await app.fetch(new Request('http://localhost/b'))
+  
+      assertEquals(
+        await a.text(),
+        '<h1>hello world</h1>',
+      )
+      assertEquals(
+        a.headers.get('content-type'),
+        'text/html; charset=utf-8',
+      )
+      assertEquals(
+        await b.text(),
+        '<h1>hello world</h1>',
+      )
+      assertEquals(
+        b.headers.get('content-type'),
+        'text/html; charset=utf-8',
+      )
+    })
 
-    const a = await app.fetch(new Request('http://localhost/a'))
-    const b = await app.fetch(new Request('http://localhost/b'))
+    await t.step('styled', async () => {
+      const app = new cheetah();
 
-    assertEquals(
-      await a.text(),
-      '<h1>hello world</h1>',
-    )
-    assertEquals(
-      a.headers.get('content-type'),
-      'text/html; charset=utf-8',
-    )
-    assertEquals(
-      await b.text(),
-      '<h1>hello world</h1>',
-    )
-    assertEquals(
-      b.headers.get('content-type'),
-      'text/html; charset=utf-8',
-    )
+      const twindConfig = defineConfig({
+        presets: [presetAutoPrefix(), presetTailwind()],
+      });
+
+      install(twindConfig)
+
+      function Styled() {
+        return <h3 class="text-sm italic" id="styled">styled <code class="font-mono">h3</code> component</h3>
+      }
+
+      app.get('/a', (c) => jsx(c, Styled))
+  
+      const a = await app.fetch(new Request('http://localhost/a'))
+
+      const document = new DOMParser().parseFromString(await a.text(), "text/html");
+
+      assert(document)
+      assert([...document.getElementsByTagName('style')].length)
+      assertEquals(document.getElementById('styled')?.innerText, 'styled h3 component')
+      assertEquals(
+        a.headers.get('content-type'),
+        'text/html; charset=utf-8',
+      )
+    })
   })
 })
