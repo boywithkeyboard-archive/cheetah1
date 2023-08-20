dd0aadb8d06d1c468d1e98192a2ae26b73d375cf
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-20 02:40:37 +0200

    refactor: simplify `render` module (#185)
    * refactor(render): fmt error message
    
    * fix issues and complete update
    
    * fix lint issue

--- test/deps.ts
+++ test/deps.ts
@@ -3,10 +3,5 @@ export {
   assertEquals,
   assertInstanceOf,
 } from 'https://deno.land/std@0.198.0/assert/mod.ts'
-export { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
-export { defineConfig } from 'https://esm.sh/@twind/core@1.1.3'
-import presetAutoPrefix from 'https://esm.sh/@twind/preset-autoprefix@1.0.7'
-import presetTailwind from 'https://esm.sh/@twind/preset-tailwind@1.1.4'
-export { presetAutoPrefix, presetTailwind }
 export { DOMParser } from 'https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts'
-export { install } from 'https://esm.sh/@twind/core@1.1.3'
+export { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
