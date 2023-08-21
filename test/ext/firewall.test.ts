97354d007387684a5f83c4c71aafd2646b38d25c
ivy <ivy@not-ivy.dev>
2023-08-21 12:15:19 -0700

    feat(ext): firewall extension (#184)
    * feat: add firewall extention
    
    closes #152
    
    * test: add tetsing for firewall extention
    
    * refactor: removed customLists, use customRanges instead
    
    chore: `deno task check`
    
    * test: removed debug option and added proxy to cloudflare in firewall tests
    
    * feat: check for updates on request
    
    * chore: removed unused file
    
    fix: double checking
    
    ---------
    
    Co-authored-by: Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>

---
+++ test/ext/firewall.test.ts
@@ -0,0 +1,61 @@
+import { assertEquals } from '../deps.ts'
+import cheetah from '../../mod.ts'
+import { firewall } from '../../ext/firewall.ts'
+
+Deno.test('ext/firewall', async (t) => {
+  await t.step('vpn', async () => {
+    const app = new cheetah({ proxy: 'cloudflare' })
+
+    app.use(firewall({
+      blockVPN: true,
+    }))
+
+    const res = await app.fetch(
+      new Request('http://localhost', {
+        headers: {
+          'cf-connecting-ip': '2.56.16.0',
+        },
+      }),
+    )
+
+    assertEquals(res.status, 403)
+  })
+})
+
+Deno.test('datacenters', async () => {
+  const app = new cheetah({ proxy: 'cloudflare' })
+
+  app.use(firewall({
+    blockDatacenter: true,
+  }))
+
+  const res = await app.fetch(
+    new Request('http://localhost', {
+      headers: {
+        'cf-connecting-ip': '1.12.32.0',
+      },
+    }),
+  )
+
+  assertEquals(res.status, 403)
+})
+
+Deno.test('customRanges', async () => {
+  const app = new cheetah({ proxy: 'cloudflare' })
+
+  app.use(firewall({
+    customRanges: [
+      '1.2.3.4/32',
+    ],
+  }))
+
+  const res = await app.fetch(
+    new Request('http://localhost', {
+      headers: {
+        'cf-connecting-ip': '1.2.3.4',
+      },
+    }),
+  )
+
+  assertEquals(res.status, 403)
+})
