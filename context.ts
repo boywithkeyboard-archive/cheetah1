fb1dfaf2e5c88ef45688dc9400a2882c3259439a
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-26 23:40:10 +0200

    refactor(context): throw exception

--- context.ts
+++ context.ts
@@ -151,7 +151,7 @@ export class Context<
   exception(error: keyof typeof HTTP_MESSAGES, description?: string) {
     const code = HTTP_MESSAGES[error]
 
-    return new Exception(error, description, code)
+    throw new Exception(error, description, code)
   }
 }
 
