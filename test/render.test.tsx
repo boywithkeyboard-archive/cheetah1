80d2d5e426db2a5a08331539ab4e9266c91aefcd
JaymanMDev <jaymanmatthewsdev@gmail.com>
2023-09-04 11:27:18 +1000

    update render tests to include 3 steps (1 with Twind styling, 1 without Twind styling to test for empty style tag injection, and 1 to test for meta tag injection in head.)

--- test/render.test.tsx
+++ test/render.test.tsx
@@ -1,70 +1,114 @@
 // Copyright 2023 Samuel Kopp. All rights reserved. Apache-2.0 license.
 /** @jsx h */
+import { VNode } from 'https://esm.sh/v128/preact@10.17.1'
 import cheetah, { h, Renderer } from '../mod.ts'
-import { assert, assertEquals, DOMParser } from './deps.ts'
+import { assert, assertEquals, DOMParser, z } from './deps.ts'
 
-Deno.test('render', async () => {
-  const app = new cheetah()
+const Document = ({ children }: { children: VNode }) => {
+  return (
+    <html>
+      <head>
+        <title>This is a document!</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
 
-  const { render } = new Renderer()
-
-  function Styled() {
-    return (
+const Styled = () => {
+  return (
+    <Document>
       <h3 class='text-sm italic' id='styled'>
         styled <code class='font-mono'>h3</code> component
       </h3>
-    )
-  }
-
-  app.get('/a', (c) => render(c, <Styled />))
-
-  const a = await app.fetch(new Request('http://localhost/a'))
-
-  const tx = await a.text()
-
-  const document = new DOMParser().parseFromString(
-    tx,
-    'text/html',
+    </Document>
   )
+}
 
-  assert(document)
-  assert([...document.getElementsByTagName('style')].length)
-  assertEquals(
-    document.getElementById('styled')?.innerText,
-    'styled h3 component',
+const Unstyled = () => {
+  return (
+    <Document>
+      <h3 id='unstyled'>
+        unstyled <code>h3</code> component
+      </h3>
+    </Document>
   )
-  assertEquals(
-    a.headers.get('content-type'),
-    'text/html; charset=utf-8',
-  )
-})
+}
 
-Deno.test('No empty style tag is injected if no twind styles are utilised.', async () => {
+Deno.test('render', async (test) => {
   const app = new cheetah()
 
   const { render } = new Renderer()
 
-  function NonStyled() {
-    return (
-      <h3 id='styled'>
-        non-styled <code>h3</code> component
-      </h3>
+  app.get('/render', {
+    query: z.object({
+      type: z.union([z.literal('styled'), z.literal('unstyled')]),
+    }),
+  }, (ctx) => {
+    render(
+      ctx,
+      ctx.req.query.type === 'styled' ? <Styled /> : <Unstyled />,
     )
-  }
+  })
 
-  app.get('/a', (c) => render(c, <NonStyled />))
+  await test.step('Twind styles are applied to the resulting HTML correctly', async () => {
+    const renderResponse = await app.fetch(
+      new Request('http://localhost/render?type=styled'),
+    )
+    const htmlText = await renderResponse.text()
+    const document = new DOMParser().parseFromString(
+      htmlText,
+      'text/html',
+    )
+    assert(document)
+    assert([...document.getElementsByTagName('style')].length === 1)
+    assertEquals(
+      document.getElementById('styled')?.innerText,
+      'styled h3 component',
+    )
+    assertEquals(
+      renderResponse.headers.get('content-type'),
+      'text/html; charset=utf-8',
+    )
+  })
 
-  const a = await app.fetch(new Request('http://localhost/a'))
-
-  const tx = await a.text()
-
+  const renderResponse = await app.fetch(
+    new Request('http://localhost/render?type=unstyled'),
+  )
+  const htmlText = await renderResponse.text()
   const document = new DOMParser().parseFromString(
-    tx,
+    htmlText,
     'text/html',
   )
+
   assert(document)
-  assertEquals(
-    [...document.getElementsByTagName('style')].length,
-    0,
-  )
+  await test.step('No empty style tag is injected if no Twind styles are utilised.', () => {
+    assertEquals(
+      [...document.getElementsByTagName('style')].length,
+      0,
+    )
+    assertEquals(
+      document.getElementById('unstyled')?.innerText,
+      'unstyled h3 component',
+    )
+    assertEquals(
+      renderResponse.headers.get('content-type'),
+      'text/html; charset=utf-8',
+    )
+  })
+
+  await test.step('Head meta tags are able to be injected into the HTML output properly.', () => {
+    const headElementsInDocument = [...document.getElementsByTagName('head')]
+    assert(headElementsInDocument.length === 1)
+    const headElementInDocument = headElementsInDocument.at(0)
+    assert(headElementInDocument)
+    assert(
+      [...headElementInDocument.children].find((childNode) =>
+        childNode.tagName === 'TITLE' &&
+        childNode.textContent === 'This is a document!'
+      ),
+    )
+  })
 })
